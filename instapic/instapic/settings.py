87446ef66ac5532ef22c48b580f56735b288968b
SalmanSaleem1 <salman.saleem@southvillesol.com>
2019-04-22 20:33:25 +0000

    remove STATICFILES_STORAGE

--- instapic/instapic/settings.py
+++ instapic/instapic/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'n4xt&xp)udgml=72z@+ra@%x)8*4$(sru+3@zr_p8iw7um0tsd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['protected-plains-87938.herokuapp.com']
+# ALLOWED_HOSTS = ['protected-plains-87938.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'instapic.instapic.urls'
+ROOT_URLCONF = 'instapic.urls'
 
 TEMPLATES = [
     {
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
@@ -128,6 +128,6 @@ LOGOUT_REDIRECT_URL = '/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 AUTHENTICATION_BACKENDS = ('instapic.authb.AuthB',)
